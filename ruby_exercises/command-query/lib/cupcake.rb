c19f7df62e22262ad8134b8e33c92bcabb787702
Madelyn Romero <m.romero013@gmail.com>
2025-01-28 20:02:39 -0700

    Create Cupcake Class

---
+++ ruby_exercises/command-query/lib/cupcake.rb
@@ -0,0 +1,9 @@
+class Cupcake
+    attr_reader :flavor,
+                :grams_of_sugar
+
+    def initialize(flavor, grams_of_sugar)
+        @flavor = flavor
+        @grams_of_sugar = grams_of_sugar
+    end
+end
